4a8a0829faa758fe811e87f24f11a6cd0be20645
Evandro Peres <evandro_peres@hotmail.com>
2020-04-06 22:15:35 -0300

    licao javascript basica

---
+++ js/main.js
@@ -0,0 +1,94 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "TESTE OK TESTE OK TESTE OK TESTE OK TESTE OK TESTE OK TESTE OK TESTE OK TESTE OK";
+    console.log()
+}
+
+function imagem(){
+    document.getElementById("imagem").innerHTML = "<h1>IMAGEM ACEITA</h1>";
+}
+
+function redirecionar(){
+    window.open("http://www.google.com.br/");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "troca troca"
+    
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse"
+}
+
+function load(){
+    alert("teste")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+//function soma(n1,n2){
+ //   return n1+n2;
+//}
+
+//function validaIdade(idade){
+  //      var validar = true;
+    //    if(idade >= 18){
+      //      validar=true
+        //}else{
+          //  validar=false
+        //}
+        //return validar;
+//}
+
+//var idade = prompt("Qual sua idade");
+//console.log(validaIdade(idade));
+
+
+//var d = new Date();w
+//alert(d.getDate()+ "/" + (d.getMonth()+1));
+
+/*var count;
+for(count=0; count < 5; count++){
+    alert(count);
+}*/
+
+
+
+/*var count = 0;
+while (count < 8){
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+//var idade = prompt("Qual sua idade?");
+//if (idade > 18){
+//    alert("maior de idade");
+//} else{
+//    alert("menor de idade");
+//}
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", coe:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+//var fruta = {nome:"maça", cor:"vermelha"};
+//console.log(fruta.nome);
+//alert(fruta.cor);*/
+
+//var lista = ["maça","pêra","laranja"];
+//lista.push();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" * "))
+//var nome = "Evandro Lopes";
+//var idade = 29+1;
+//var idade2 = 4;
+//var frase="Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.replace("Japão","Brasilsilsilsil"));
+//console.log(frase.toUpperCase());
